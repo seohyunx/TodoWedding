029171f0ad02d2c057827a76d1f682c7e2fad3b3
seohyunx <sh0501sh11@gmail.com>
2024-01-09 21:56:31 +0900

    feat: 결혼일 조회 에러 처리 수정

--- frontend/todowedding/src/components/Header/Header.jsx
+++ frontend/todowedding/src/components/Header/Header.jsx
@@ -45,9 +45,6 @@ const Header = () => {
           let date = res.data * -1;
           setMarryDt("+" + date);
         }
-      })
-      .catch((err) => {
-        console.log("결혼일 조회 에러 : ", err);
       });
   };
 
